6a19faf388402f916b1d8803f9a8fc089778b281
aleroms <santiagomorales333@yahoo.com>
2024-05-20 19:18:52 -0700

    added go back buttons and preview to profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>profile page</div>
+  <section class="profile-container">
+    <h1>My Profile</h1>
+    <p>This is where you can add, remove and update information from your linktree.</p>
+  </section>
+  <RouterLink to="/">go back</RouterLink>
+  <RouterLink to="/" target="_blank">preview</RouterLink>
 </template>
 
 <script setup lang="ts"></script>
